5b88b11a7c78c9eba649427af6f761bd2264d960
ManmathGantayat <54932740+ManmathGantayat@users.noreply.github.com>
2026-02-21 23:17:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const app = require('./app');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
 });
